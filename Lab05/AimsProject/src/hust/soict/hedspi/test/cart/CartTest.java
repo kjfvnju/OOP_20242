6b382874abcf5eaa2f2b1e312ab455302749e1e4
kjfvnju <hoangvuhuy.3006@gmail.com>
2025-05-24 22:58:30 +0700

    final!

---
+++ Lab05/AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
@@ -0,0 +1,20 @@
+package hust.soict.hedspi.test.cart;
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+
+        cart.print();
+        cart.searchById(1);
+        cart.searchByTitle("aladin");
+    }
+}
+
